e8dbfa67818204d83c1ea487bfe6807196cab005
Junior-Stranner <Ju-stranner@outlook.com>
2024-02-26 15:34:16 -0300

    start the game

---
+++ jogo-da-velha/src/Campo.java
@@ -0,0 +1,19 @@
+public class Campo {
+    private char simbolo;
+
+    public Campo(){
+      this.simbolo = ' ';
+    }
+
+    public char getSimbolo(){
+        return simbolo;
+    }
+
+    public void setSimbolo(char simbolo){
+      if(this.simbolo == ' '){
+        this.simbolo = simbolo;
+      }else{
+        System.out.println("Campo já usado !");
+      }
+    }
+}
---
+++ jogo-da-velha/src/JogoDaVelha.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class JogoDaVelha {
+    public static void main(String []args){
+
+    Scanner scanner = new Scanner(System.in);
+      Campo[][] Velha = new Campo[3][3];
+      char simboloAtual = 'x';
+      boolean game = true;
+      String vitoria = "";
+
+      while (game) {
+        
+      }
+    }
+
+    public static void desenhaJogo(){
+        
+    }
+}
